a00b1be9f4fadf5dc466f56a72d3bf910d2a524e
rbd3 <rabedesanaa@gmail.com>
2023-08-17 19:35:56 +0300

    Add data

--- schema.sql
+++ schema.sql
@@ -24,4 +24,20 @@ ALTER TABLE animals ADD CONSTRAINT fk_animals_species FOREIGN KEY (species_id) R
 ALTER TABLE animals ADD COLUMN owner_id INT;
 ALTER TABLE animals ADD CONSTRAINT fk_animals_owner FOREIGN KEY (owner_id) REFERENCES owners (id);
 
---
+--VETS TABLE
+CREATE TABLE vets (id INT GENERATED ALWAYS AS IDENTITY, 
+name VARCHAR(100), 
+age INT, date_of_graduation DATE, 
+PRIMARY KEY(id));
+
+--SPECIALISATION TABLE
+CREATE TABLE specializations (vet_id INT REFERENCES vets(id), 
+species_id INT REFERENCES species(id), 
+PRIMARY KEY (vet_id, species_id));
+
+--visits
+CREATE TABLE visits (vet_id INT REFERENCES vets(id), 
+animal_id INT REFERENCES animals(id), date_of_visit DATE, 
+PRIMARY KEY (vet_id, animal_id));
+ALTER TABLE visits DROP CONSTRAINT visits_pkey; --i can't add multiple visits
+
